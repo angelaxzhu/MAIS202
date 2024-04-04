46718842e8e41a740837767f746cbaca5ac9730a
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2024-04-03 21:14:55 -0400

    azm training done, done predict/find unitigs/generate test sequence

--- Code/final_model.py
+++ Code/final_model.py
@@ -1,18 +1,80 @@
-def predict_azm(unitigs):
-    return result
-def predict_cfx(unitigs):
+import joblib
+import numpy as np
+import pandas as pd
+
+#note: find is not the best algorithm cuz it can include overlapping sequences... 
+
+def predict_azm(seq):
+    unitigs = find_unitigs(seq,"seq")
+    loaded_rf = joblib.load("./random_forest_azm.joblib")
+    result = loaded_rf.predict(unitigs)
     return result
 
-def predict_cip(unitigs):
+def predict_cfx(seq):
+    unitigs = find_unitigs(seq,"cfx")
+    loaded_rf = joblib.load("./random_forest_cfx.joblib")
+    result = loaded_rf.predict(unitigs)
     return result
-def find_unitigs(seq):
+
+def predict_cip(seq):
+    unitigs = find_unitigs(seq,"cip")
+    loaded_rf = joblib.load("./random_forest_cip.joblib")
+    result = loaded_rf.predict(unitigs)
+    return result
+
+def find_unitigs(seq,ab):
+    unitigs_list = []
+    if ab == "azm":
+        unitigs = []
+        #csv to list
+        unitigs_file = open(r'.\Data\azm_unitigs.txt','r')
+        #remove linebreak
+        for line in unitigs_file:
+            uni = line[:-1]
+            unitigs.append(uni)
+
+        for u in unitigs:
+            if seq.find(u) == -1: 
+                unitigs_list.append(0)
+            else:
+                unitigs_list.append(1)
+
+    elif ab == "cfx":
+        unitigs = []
+        #csv to list
+        unitigs_file = open(r'.\Data\cfx_unitigs.txt','r')
+        #remove linebreak
+        for line in unitigs_file:
+            uni = line[:-1]
+            unitigs.append(uni)
+
+        for u in unitigs:
+            if seq.find(u) == -1: 
+                unitigs_list.append(0)
+            else:
+                unitigs_list.append(1)
+
+    else: 
+        unitigs = []
+        #csv to list
+        unitigs_file = open(r'.\Data\cip_unitigs.txt','r')
+        #remove linebreak
+        for line in unitigs_file:
+            uni = line[:-1]
+            unitigs.append(uni)
+
+        for u in unitigs:
+            if seq.find(u) == -1: 
+                unitigs_list.append(0)
+            else:
+                unitigs_list.append(1)
+
     return unitigs_list
 
 def model_test(seq):
-    unitigs = find_unitigs(seq)
-    results_azm = predict_azm(unitigs)
-    results_cip = predict_cip(unitigs)
-    results_cfx = predict_cfx(unitigs)
+    results_azm = predict_azm(seq)
+    results_cip = predict_cip(seq)
+    results_cfx = predict_cfx(seq)
 
     results = []
     results.append(results_azm)
@@ -20,4 +82,20 @@ def model_test(seq):
     results.append(results_cfx)
 
     return results
+
+#make test sample for azm 
+def generate_test():
+    seq=""
+    unitigs_l = pd.read_csv('Data\zm_sr_gwas_filtered_unitigs.Rtab',sep=' ',index_col=0)
+    unitigs_list = (unitigs_l['ERR1549286']).tolist()
+    unitigs_n = unitigs_l.index.values.tolist()
+    print(unitigs_l)
+    print('unitigs',unitigs_list)   
+    i = 0
+    while i < len(unitigs_list):
+        if unitigs_list[i]==1:
+            seq = seq + unitigs_n[i]
+        i = i + 1
+
+    return seq
 
---
+++ Code/training.py
@@ -0,0 +1,180 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+import joblib
+
+
+#this program closely follows the steps delineated in the tutorial:https://www.datacamp.com/tutorial/random-forests-classifier-python
+def train_azm():
+    #1. Load data
+    labels_azm = pd.read_csv('Data\metadata.csv',usecols=["Sample_ID","azm_sr"])
+    features_azm = pd.read_csv('Data\zm_sr_gwas_filtered_unitigs.Rtab',sep=' ',index_col=0)
+
+
+    #2. Remove N/A 
+    to_delete =[]
+    s = 0 
+    while s < len(labels_azm): #gives the number of rows --> number of samples
+        if np.isnan(labels_azm.loc[s].at["azm_sr"]) :
+            to_delete.append(labels_azm.loc[s].at["Sample_ID"])
+            labels_azm.drop(index=s,inplace=True)
+        s = s +1
+
+    #3. inner join between the two dataframes to only keep complete data
+    transposed_features_azm = features_azm.transpose();
+    joined = pd.merge(labels_azm,transposed_features_azm,how='inner',left_on="Sample_ID",right_index=True)
+
+
+    #4. un-join to separate into X and Y
+    #get list of unitigs titles so we can use the index when predicting => but remove it from the dataset itself ...or can just refer to index... hm, maybe not necessary
+
+  
+    processed_labels = joined['azm_sr']
+    processed_features = joined.drop(['Sample_ID','azm_sr'],axis=1)
+    unitigs = processed_features.columns
+
+    #Splitting the data
+    features_train, features_test, labels_train, labels_test = train_test_split(processed_features,processed_labels,test_size=0.67) 
+    
+
+    #Training the model
+    rf = RandomForestClassifier()
+    rf.fit(features_train,labels_train)
+    joblib.dump(rf,"./random_forest_azm.joblib")
+
+
+    #Predict with testing set
+    labels_prediction = rf.predict(features_test)
+    print(labels_prediction)
+
+    #Metrics
+    accuracy = accuracy_score(labels_prediction, labels_test)
+    precision = precision_score(labels_test, labels_prediction)
+    recall = recall_score(labels_test,labels_prediction)
+    print(accuracy)
+    print(precision)
+    print(recall)
+    c_m = confusion_matrix(labels_test,labels_prediction)
+    ConfusionMatrixDisplay(confusion_matrix=c_m).plot()
+    plt.show()
+    #save unitigs
+    with open(r'.\Data\azm_unitigs.txt','w') as file:
+        for unitig in unitigs:
+            file.write("%s\n" % unitig)
+    file.close()
+    
+    return unitigs
+
+def train_cip():
+     #1. Load data
+    labels_azm = pd.read_csv('Data\metadata.csv',usecols=["Sample_ID","cip_sr"])
+    features_azm = pd.read_csv('Data\cip_sr_gwas_filtered_unitigs.Rtab',sep=' ',index_col=0)
+
+
+    #2. Remove N/A 
+    to_delete =[]
+    s = 0 
+    while s < len(labels_azm): #gives the number of rows --> number of samples
+        if np.isnan(labels_azm.loc[s].at["cip_sr"]) :
+            to_delete.append(labels_azm.loc[s].at["Sample_ID"])
+            labels_azm.drop(index=s,inplace=True)
+        s = s +1
+
+    #3. inner join between the two dataframes to only keep complete data
+    transposed_features_azm = features_azm.transpose();
+    joined = pd.merge(labels_azm,transposed_features_azm,how='inner',left_on="Sample_ID",right_index=True)
+
+
+    #4. un-join to separate into X and Y
+    #get list of unitigs titles so we can use the index when predicting => but remove it from the dataset itself ...or can just refer to index... hm, maybe not necessary
+
+  
+    processed_labels = joined['cip_sr']
+    processed_features = joined.drop(['Sample_ID','cip_sr'],axis=1)
+    unitigs = processed_features.columns
+
+    #Splitting the data
+    features_train, features_test, labels_train, labels_test = train_test_split(processed_features,processed_labels,test_size=0.67) 
+    
+
+    #Training the model
+    rf = RandomForestClassifier()
+    rf.fit(features_train,labels_train)
+
+    joblib.dump(rf,"./random_forest_cip.joblib")
+
+    #Predict with testing set
+    labels_prediction = rf.predict(features_test)
+    print(labels_prediction)
+
+    #Metrics
+    accuracy = accuracy_score(labels_prediction, labels_test)
+    precision = precision_score(labels_test, labels_prediction)
+    recall = recall_score(labels_test,labels_prediction)
+    print(accuracy)
+    print(precision)
+    print(recall)
+    c_m = confusion_matrix(labels_test,labels_prediction)
+    ConfusionMatrixDisplay(confusion_matrix=c_m).plot()
+    plt.show()
+    return unitigs
+def train_cfx():
+     #1. Load data
+    labels_azm = pd.read_csv('Data\metadata.csv',usecols=["Sample_ID","cfx_sr"])
+    features_azm = pd.read_csv('Data\cfx_sr_gwas_filtered_unitigs.Rtab',sep=' ',index_col=0)
+
+
+    #2. Remove N/A 
+    to_delete =[]
+    s = 0 
+    while s < len(labels_azm): #gives the number of rows --> number of samples
+        if np.isnan(labels_azm.loc[s].at["cfx_sr"]) :
+            to_delete.append(labels_azm.loc[s].at["Sample_ID"])
+            labels_azm.drop(index=s,inplace=True)
+        s = s +1
+
+    #3. inner join between the two dataframes to only keep complete data
+    transposed_features_azm = features_azm.transpose();
+    joined = pd.merge(labels_azm,transposed_features_azm,how='inner',left_on="Sample_ID",right_index=True)
+
+
+    #4. un-join to separate into X and Y
+    #get list of unitigs titles so we can use the index when predicting => but remove it from the dataset itself ...or can just refer to index... hm, maybe not necessary
+
+  
+    processed_labels = joined['cfx_sr']
+    processed_features = joined.drop(['Sample_ID','cfx_sr'],axis=1)
+    unitigs = processed_features.columns
+
+    #Splitting the data
+    features_train, features_test, labels_train, labels_test = train_test_split(processed_features,processed_labels,test_size=0.67) 
+    
+
+    #Training the model
+    rf = RandomForestClassifier()
+    rf.fit(features_train,labels_train)  
+    joblib.dump(rf,"./random_forest_cfx.joblib")    
+
+
+    #Predict with testing set
+    labels_prediction = rf.predict(features_test)
+    print(labels_prediction)
+
+    #Metrics
+    accuracy = accuracy_score(labels_prediction, labels_test)
+    precision = precision_score(labels_test, labels_prediction)
+    recall = recall_score(labels_test,labels_prediction)
+    print(accuracy)
+    print(precision)
+    print(recall)
+    c_m = confusion_matrix(labels_test,labels_prediction)
+    ConfusionMatrixDisplay(confusion_matrix=c_m).plot()
+    plt.show()
+    return unitigs
+   
+train_azm()
+# #train_cip()
+#train_cfx()
